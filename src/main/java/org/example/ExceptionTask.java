c1996a037f01696a7f878c862817343567c67378
NestyChe <oldschoolage13@gmail.com>
2022-10-31 18:37:32 +0400

    add class print exception

---
+++ src/main/java/org/example/ExceptionTask.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ExceptionTask {
+
+    public static void descriptionExcept(){
+        System.err.println("input description");
+    }
+
+    public static void wrongInputExcept(){
+        System.err.println("wrong id, no task with this id");
+    }
+}
