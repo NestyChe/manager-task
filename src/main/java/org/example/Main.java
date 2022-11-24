132a7574eb3736aef1a0348ce2af30bd6aae9db5
NestyChe <oldschoolage13@gmail.com>
2022-11-24 13:12:15 +0400

    add logger,set logger level program arguments to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
-
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Scanner;
-
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
     static Commands commands = new Commands();
 
     public static void main(String[] args) {
         final Scanner in = new Scanner(System.in);
+        org.apache.logging.log4j.core.config.Configurator.setLevel(LogManager.getRootLogger(),
+                Level.valueOf(args[0].toUpperCase()));
         while(in.hasNext()) {
             String command = in.next();
             String task = in.nextLine().trim();
+            logger.debug("User input: {} {}", command, task);
             if (command.equals("add")) {
                 commands.add(task);
             } else if (command.equals("quit")) {
@@ -35,10 +41,12 @@ public class Main {
                         commands.delete(task);
                         break;
                     default:
+                        logger.error("unknown command");
                         System.err.println("unknown command");
                         break;
                 }
             } else {
+                logger.error("task list empty");
                 System.err.println("please, save task");
             }
         }
