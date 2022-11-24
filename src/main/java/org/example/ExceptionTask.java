159c3a961c0c937fddd31b71e9eec50de1adc36b
NestyChe <oldschoolage13@gmail.com>
2022-11-24 13:03:59 +0400

    add logger to exceptions

--- src/main/java/org/example/ExceptionTask.java
+++ src/main/java/org/example/ExceptionTask.java
@@ -1,12 +1,20 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.NoSuchElementException;
+
 public class ExceptionTask {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionTask.class.getName());
     public static void descriptionExcept(){
+        logger.error("Пустое значение id");
         System.err.println("input description");
     }
 
-    public static void wrongInputExcept(){
+    public static void wrongInputExcept(String id, NoSuchElementException exception){
+        logger.error("Нет задачи с таким  id: {} и exception: {}", id, exception.getLocalizedMessage());
         System.err.println("wrong id, no task with this id");
     }
 }
